1ec20a60bd2f85a4a5d157e00a7043d41798202d
aokorodu <aokorodu@gmail.com>
2023-12-22 19:46:12 -0500

    second pass, looks great

--- src/components/bezierdemo/index.tsx
+++ src/components/bezierdemo/index.tsx
@@ -4,28 +4,45 @@ import { useEffect, useState, useRef } from "react"
 
 const BezierDemo = () => {
 
-    const path = useRef(null);
-    const ball = useRef(null);
-    const stage = useRef<SVGGraphicsElement>(null);
+    const path = useRef<SVGPathElement>(null);
+    const ball = useRef<SVGElement>(null);
+    const stage = useRef<SVGSVGElement>(null);
     let dragging = false;
     const startDragging = () => {
         dragging = true;
 
         stage.current?.addEventListener("mousemove", drag);
-        stage.current?.addEventListener("mouseup", stopDragging)
+        stage.current?.addEventListener("mouseup", stopDragging);
 
     }
 
 
     const drag = (e: MouseEvent) => {
+        if (!dragging) return;
+
         const x = e.clientX;
         const y = e.clientY;
 
-        if (stage.current != null) {
-            const p = toSVGPoint(stage.current, x, y);
-            console.log('p ', p);
-        }
 
+        if (stage.current != null) {
+
+            const p = toSVGPoint(stage.current, x, y);
+            console.log('p ', p.y);
+            if ((p.x < 20) || (p.x > 480) || (p.y < 20) || (p.y > 480)) {
+                stopDragging();
+                return;
+            }
+
+            ball.current?.setAttribute("cx", String(p.x));
+            ball.current?.setAttribute("cy", String(p.y));
+
+            updatePath(p.x, p.y);
+        }
+    }
+
+    const updatePath = (x: number, y: number) => {
+        const str = `M 0,250 L 50,250 Q ${x},${y} 450,250 L 500,250`;
+        path.current?.setAttribute("d", str)
     }
 
     const toSVGPoint = (svg: SVGGraphicsElement, x: number, y: number) => {
@@ -37,6 +54,7 @@ const BezierDemo = () => {
 
     const stopDragging = () => {
         dragging = false;
+        console.log('stop dragging')
         stage.current?.removeEventListener("mousemove", drag)
         stage.current?.removeEventListener("mouseup", stopDragging)
     }
@@ -44,7 +62,9 @@ const BezierDemo = () => {
     return (<>
         <div>hello world</div>
         <svg ref={stage} width={500} height={500} viewBox="0 0 500 500">
+            <rect x={20} y={20} width={460} height={460} fill={"#eaeaea"} stroke={"none"} />
             <path ref={path} d="M 0,250 L 50,250 Q 250,120 450,250 L 500,250" stroke={"black"} strokeWidth={2} fill="none" />
+
             <circle
                 onMouseDown={() => {
                     startDragging();
