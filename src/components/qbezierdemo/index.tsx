f5834833ab8de4aa666506943598380b1170a42a
aokorodu <aokorodu@gmail.com>
2023-12-22 20:58:58 -0500

    more updates

--- src/components/qbezierdemo/index.tsx
+++ src/components/qbezierdemo/index.tsx
@@ -7,7 +7,7 @@ const QBezierDemo = () => {
     const path = useRef<SVGPathElement>(null);
     const ball = useRef<SVGCircleElement>(null);
     const stage = useRef<SVGSVGElement>(null);
-    const pathText = useRef<SVGTextElement>(null)
+    const pathText = useRef<SVGTSpanElement>(null)
     let dragging = false;
     const startDragging = () => {
         dragging = true;
@@ -44,7 +44,7 @@ const QBezierDemo = () => {
     const updatePath = (x: number, y: number) => {
         const str = `M 0,250 L 50,250 Q ${Math.round(x)},${Math.round(y)} 450,250 L 500,250`;
         path.current?.setAttribute("d", str);
-        if (pathText.current != null) pathText.current.textContent = str;
+        if (pathText.current != null) pathText.current.textContent = `Q ${Math.round(x)}, ${Math.round(y)}`;
     }
 
     const toSVGPoint = (svg: SVGGraphicsElement, x: number, y: number) => {
@@ -66,7 +66,7 @@ const QBezierDemo = () => {
             <rect x={20} y={20} width={460} height={460} fill={"#eaeaea"} stroke={"none"} />
             <path ref={path} d="M 0,250 L 50,250 Q 250,120 450,250 L 500,250" stroke={"black"} strokeWidth={2} fill="none" />
             <text x={250} y={100} textAnchor={"middle"}>Drag Me!</text>
-            <text ref={pathText} x={250} y={450} textAnchor={"middle"}>"M 0,250 L 50,250 Q 250,120 450,250 L 500,250"</text>
+            <text x={250} y={450} textAnchor={"middle"}>"M 0,250 L 50,250 <tspan stroke={"red"} ref={pathText}>Q 250, 120</tspan> 450,250 L 500,250"</text>
             <circle
                 onMouseDown={() => {
                     startDragging();
