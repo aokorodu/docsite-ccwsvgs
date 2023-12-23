3edb20b3deb14508250ab58d215c1372d0c4ac08
aokorodu <aokorodu@gmail.com>
2023-12-23 11:54:31 -0500

    full functionality

--- src/components/tbezierdemo/index.tsx
+++ src/components/tbezierdemo/index.tsx
@@ -7,6 +7,7 @@ const TBezierDemo = () => {
     const path = useRef<SVGPathElement>(null);
     const ball = useRef<SVGCircleElement>(null);
     const ballReflection = useRef<SVGCircleElement>(null);
+    const connector = useRef<SVGPathElement>(null);
     const stage = useRef<SVGSVGElement>(null);
     const pathText = useRef<SVGTSpanElement>(null)
     let dragging = false;
@@ -43,7 +44,7 @@ const TBezierDemo = () => {
             ball.current?.setAttribute("cy", String(ypos));
             ballReflection.current?.setAttribute("cx", String(xposMirror));
             ballReflection.current?.setAttribute("cy", String(yposMirror));
-
+            connector.current?.setAttribute("d", `M${xpos},${ypos} L${xposMirror},${yposMirror}`);
             updatePath(xpos, ypos);
         }
     }
@@ -51,7 +52,7 @@ const TBezierDemo = () => {
     const updatePath = (x: number, y: number) => {
         const str = `M 0,250 Q ${Math.round(x)},${Math.round(y)} 250,250 T 500,250`;
         path.current?.setAttribute("d", str);
-        if (pathText.current != null) pathText.current.textContent = `Q ${Math.round(x)}, ${Math.round(y)} T`;
+        if (pathText.current != null) pathText.current.textContent = `Q ${Math.round(x)}, ${Math.round(y)} 250,250 T`;
     }
 
     const toSVGPoint = (svg: SVGGraphicsElement, x: number, y: number) => {
@@ -72,8 +73,11 @@ const TBezierDemo = () => {
         <svg ref={stage} width={500} height={500} viewBox="0 0 500 500">
             <rect x={20} y={20} width={460} height={460} fill={"#eaeaea"} stroke={"none"} />
             <path ref={path} d="M 0,250 Q 125,100 250,250 T 500,250" stroke={"black"} strokeWidth={2} fill="none" />
-            <text x={250} y={100} textAnchor={"middle"}>Drag Me!</text>
+            <text x={125} y={75} textAnchor={"middle"}>Drag Me!</text>
             <text x={250} y={450} textAnchor={"middle"}>"M 0,250 <tspan stroke={"red"} ref={pathText}>Q 250, 120 250, 250 T</tspan> 500,250"</text>
+            <path ref={connector} d={"M125,100 L375,400"} stroke={"black"} strokeDasharray={5} />
+            <circle
+                cx={250} cy={250} r={5} fill={"black"} />
             <circle
                 onMouseDown={() => {
                     startDragging();
