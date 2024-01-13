16c7cdcd8895da2b02ff85ee10a90f53fb38304b
aokorodu <aokorodu@gmail.com>
2024-01-13 13:26:27 -0500

    particles and stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from 'react'
 import BGAnimation from '../components/bganimation'
+import styles from './page.module.css'
 
 export default function Home() {
   const anim = useRef<BGAnimation>(null);
@@ -18,7 +19,40 @@ export default function Home() {
   }, [])
   return (
     <>
-      <BGAnimation ref={anim} />
+      <div><h1>Creative Coding with SVGs</h1></div>
+
+      <div className={styles.particleHolder}>
+        <BGAnimation ref={anim} />
+      </div>
+
+      <p>Welcome to the world of SVGs - one of the most powerful and versatile visual format on the web today. SVGs allow you to create all kinds of interesting visualizations - from simple images to interactive infographics, animations, and even games.</p>
+      <p>
+        In this course, you'll learn:
+        the elements and syntax of an SVG
+        creating SVGs:
+        <ul>
+          <li>
+            using free drawing tools like Figma
+          </li>
+          <li>
+            "by hand" (in code using SVG markup)
+          </li>
+          <li>
+            dynamically using JavaScript
+          </li>
+          <li>
+            Styling SVGs using presentation attributes and CSS
+
+          </li>
+          <li>How to make SVGs interactive</li>
+          <li> How to animate SVGs</li>
+        </ul>
+
+
+
+
+
+      </p>
     </>
   )
 }
