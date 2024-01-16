1e34a615fc96a76563e708325e370e0cbb0021d8
aokorodu <aokorodu@gmail.com>
2024-01-16 18:31:19 -0500

    responsive menu first pass

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useState } from 'react';
 import { navConfig } from '../../config';
 import NavButton from "./navbutton";
 import styles from "./navigation.module.scss";
@@ -5,6 +8,7 @@ import styles from "./navigation.module.scss";
 const Navigation = () => {
     const nav = navConfig;
     const pages = nav.pages;
+    const [open, toggleOpen] = useState(false);
 
     const getPages = () => {
         console.log('get pages')
@@ -14,7 +18,18 @@ const Navigation = () => {
     };
     return (
         <>
-            <div className={styles.container}>{getPages()}</div>
+            <div className={styles.desktopContainer}>{getPages()}</div>
+            <div className={styles.mobileNavHolder}>
+                <div className={styles.hamburgerHolder} onClick={() => { console.log('click'); toggleOpen(!open) }}>x</div>
+                {open && <div className={styles.container}>
+                    <div className={styles.bg} />
+                    <div className={styles.mobileButtonContainer}>
+                        {getPages()}
+                    </div>
+                </div>}
+
+
+            </div>
         </>
     );
 };
