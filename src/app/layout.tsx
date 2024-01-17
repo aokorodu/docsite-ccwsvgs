671c8cb17909028144ae58da48a4c53a53a1f317
aokorodu <aokorodu@gmail.com>
2024-01-16 20:54:07 -0500

    minor styling tweaks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css';
 import Navigation from '@/components/navigation';
 import styles from './layout.module.scss';
+import AppHeader from '@/components/appheader';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className={styles.holder}>
+          <AppHeader />
           <Navigation />
           <div className={styles.contentHolder}>{children}</div>
         </div>
---
+++ src/components/appheader/index.tsx
@@ -0,0 +1,11 @@
+import styles from './AppHeader.module.scss';
+
+const AppHeader = () => {
+    return (
+        <><header className={styles.container}>
+            <div className={styles.title}>Creative Coding with SVGs</div>
+        </header></>
+    )
+}
+
+export default AppHeader;
