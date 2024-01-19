89c1195f5fd5933bff6eaa76b7069a0419fa8a18
aokorodu <aokorodu@gmail.com>
2024-01-18 22:16:47 -0500

    lots of tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,18 +18,22 @@ export default function Home() {
   }, [])
   return (
     <>
-      <div><h1 style={{ color: "white" }}>Creative Coding with SVGs</h1></div>
+
 
       <div className={styles.particleHolder}>
         <BGAnimation ref={anim} />
       </div>
 
-      <div style={{ color: "white" }} className={styles.subheading}>Welcome to the world of SVGs - one of the most powerful and versatile visual formats on the web today. SVGs allow you to create all kinds of interesting visualizations - from simple images to interactive infographics, animations, and even games.</div>
-      <Link href={"/intro"}>
-        <Button variant="contained" color="secondary">
-          Begin
-        </Button>
-      </Link>
+      <div className={styles.holder}>
+        <h1 style={{ color: "white" }}>Creative Coding with SVGs</h1>
+
+        <div style={{ color: "white" }} className={styles.subheading}>Welcome to the world of SVGs - one of the most powerful and versatile visual formats on the web today. SVGs allow you to create all kinds of interesting visualizations - from simple images to interactive infographics, animations, and even games.</div>
+        <Link href={"/intro"}>
+          <Button variant="contained" color="secondary">
+            Begin
+          </Button>
+        </Link>
+      </div>
     </>
   )
 }
