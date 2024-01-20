94e0dc7f37ef46471addb1bb83703a14f017d84c
aokorodu <aokorodu@gmail.com>
2024-01-20 11:35:14 -0500

    content tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,10 +27,11 @@ export default function Home() {
       <div className={styles.holder}>
         <h1 style={{ color: "white" }}>Creative Coding with SVGs</h1>
 
-        <div style={{ color: "white" }} className={styles.subheading}>Welcome to the world of SVGs - one of the most powerful and versatile visual formats on the web today. SVGs allow you to create all kinds of interesting visualizations - from simple images to interactive infographics, animations, and even games.</div>
+        <div style={{ color: "white" }} className={styles.subheading}>Welcome to the world of SVGs - one of the most powerful, versatile and dynamic visual formats available today. SVGs allow you to create all kinds of interesting visualizations - from simple images to interactive infographics, animations, and even games.</div>
+        <div style={{ color: "white" }} className={styles.subheading}>In this beginners course, we'll teach you all about the SVG and the unique creative possibilities it offers.</div>
         <Link href={"/intro"}>
           <Button variant="contained" color="secondary">
-            Begin
+            Let's Begin!
           </Button>
         </Link>
       </div>
