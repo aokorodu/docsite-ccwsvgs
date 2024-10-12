874399f8e5367e6a0d93c39622b79d30c8d39b8f
aokorodu <aokorodu@gmail.com>
2024-10-11 22:41:49 -0400

    iframe of codepen test

---
+++ src/components/matterjs/index.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+const EmbedExample = () => {
+    return (
+        <>
+            <iframe height="700px" style={{ width: "100%" }} title="MatterJS - Multiple Particles - Rectangles" src="https://codepen.io/aokorodu/embed/preview/OJKXoqd?default-tab=result&theme-id=light" frameBorder="no" loading="lazy">
+                See the Pen <a href="https://codepen.io/aokorodu/pen/OJKXoqd">
+                    MatterJS - Multiple Particles - Rectangles</a> by zuubaDigital (<a href="https://codepen.io/aokorodu">@aokorodu</a>)
+                on <a href="https://codepen.io">CodePen</a>.
+            </iframe>
+        </>
+    );
+};
+
+export default EmbedExample;
+
