f1697060c1802d85ab43ebd8d3970bafb9ac317e
aokorodu <aokorodu@gmail.com>
2026-01-19 13:08:04 -0500

    making it look cooler and adding ability to change the intro animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,72 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import BGAnimation from '@/components/bganimation';
 import styles from './Home.module.css';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
+
+const anims = ["waterFlow", "float", "sin", "orbit"];
+
+function Selector({ onClick }: { onClick: (index: number) => void }) {
+  const [selectedAnim, setSelectedAnim] = useState(0);
+  return (
+    <>
+
+      <div className={styles.changeAnimation}>
+        {anims.map((anim, index) => (
+          <>
+            <div key={anim} className={index === selectedAnim ? styles.animationName + " " + styles.selected : styles.animationName} onClick={() => { setSelectedAnim(index); onClick(index); }}><span>{anim}</span></div>
+            <span className={styles.animationName}>{index < anims.length - 1 ? "~" : ""}</span>
+          </>
+        ))}
+      </div></>
+
+  )
+}
+
 export default function Home() {
   const anim = useRef<BGAnimation>(null);
 
+  let animIndex = 0;
+  const animLength = anims.length;
+
+  const chooseRandomFlow = (isRandom: boolean) => {
+    if (!anim.current) return;
+    if (isRandom) {
+      animIndex = Math.floor(Math.random() * animLength);
+
+    } else {
+
+      animIndex = (animIndex + 1) % animLength;
+    }
+    anim.current.changeFlow(anims[animIndex]);
+  };
+
+  const chooseSpecificFlow = (index: number) => {
+    if (!anim.current) return;
+    animIndex = index % animLength;
+    anim.current.changeFlow(anims[animIndex]);
+  };
+
+  // const chooseNextFlow = () => {
+  //   if (!anim.current) return;
+  //   animIndex = (animIndex + 1) % animLength;
+  //   anim.current.changeFlow(anims[animIndex]);
+  // };
+
   useEffect(() => {
     if (anim.current) {
-      const anims = ["waterFlow", "orbit", "float", "sin"][Math.floor(Math.random() * 4)];
       anim.current.start();
-      anim.current.changeFlow(anims);
+      chooseSpecificFlow(0);
     }
   }, []);
+
+
 
   return (
     <>
 
-      <div className={styles.particleHolder}>
+      <div className={styles.particleHolder} >
         <BGAnimation ref={anim} />
       </div>
 
@@ -31,11 +79,14 @@ export default function Home() {
         <div style={{ color: "white" }} className={styles.subheading}>
           In this beginners course, we'll teach you all about the SVG and the unique creative possibilities it offers.
         </div>
-        <Link to="/intro">
-          <Button variant="contained" color="secondary">
-            Let's Begin!
-          </Button>
-        </Link>
+        <div className={styles.buttonHolder}>
+          <Link to="/intro">
+            <Button variant="contained" color="secondary">
+              Let's Begin!
+            </Button>
+          </Link>
+        </div>
+        <Selector onClick={chooseSpecificFlow} />
       </div>
     </>
   );
