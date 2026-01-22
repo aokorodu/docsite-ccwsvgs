bf73a8f0320f955b512644adaee044c82b35603c
aokorodu <aokorodu@gmail.com>
2026-01-22 13:29:18 -0500

    minor tweak to toc icon

--- src/components/tableofcontents/index.tsx
+++ src/components/tableofcontents/index.tsx
@@ -12,18 +12,9 @@ const ListIcon = () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="currentColor"
     >
-      <line x1="8" y1="6" x2="21" y2="6" />
-      <line x1="8" y1="12" x2="21" y2="12" />
-      <line x1="8" y1="18" x2="21" y2="18" />
-      <line x1="3" y1="6" x2="3.01" y2="6" />
-      <line x1="3" y1="12" x2="3.01" y2="12" />
-      <line x1="3" y1="18" x2="3.01" y2="18" />
+      <text x="12" y="12" textAnchor='middle' dominantBaseline={"middle"} style={{ font: "italic 20px serif" }} >i</text>
     </svg>
   </div>
 );
