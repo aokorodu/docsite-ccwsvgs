ae2800f89bed0c308d64c9424fbede332b327ae7
aokorodu <aokorodu@gmail.com>
2026-02-02 21:17:10 -0500

    fix the animate motion example

--- src/components/smilII/index.tsx
+++ src/components/smilII/index.tsx
@@ -1,7 +1,7 @@
 export const SuperExample = () => {
     return (
         <>
-            <svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">
+            <svg className="exampleImage" width="100%" height="500" viewBox="0 0 500 500">
 
                 <defs>
                     <g id="rocket" transform="rotate(90) translate(-10 -20)">
